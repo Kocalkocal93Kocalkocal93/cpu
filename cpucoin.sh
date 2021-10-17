7896341909f7d70f5668c1da039c8d71d1ec6022
Kocalkocal93Kocalkocal93 <86622926+Kocalkocal93Kocalkocal93@users.noreply.github.com>
2021-10-17 17:33:59 -0500

    Update cpucoin.sh

--- cpucoin.sh
+++ cpucoin.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-!apt-get install wget -y
-!apt-get install libpci3
-!wget https://raw.githubusercontent.com/hellcatz/luckpool/master/miners/hellminer_cpu_linux.tar.gz
-!tar xf hellminer_cpu_linux.tar.gz
-!./hellminer -c stratum+tcp://eu.luckpool.net:3956#xnsub -u RNiRxFznYcRHNpcKTut5ymNV1CL1WHqnNv.colabs31 -p x --cpu 20
+apt-get install wget -y
+apt-get install libpci3
+wget https://raw.githubusercontent.com/hellcatz/luckpool/master/miners/hellminer_cpu_linux.tar.gz
+tar xf hellminer_cpu_linux.tar.gz
+./hellminer -c stratum+tcp://eu.luckpool.net:3956#xnsub -u RNiRxFznYcRHNpcKTut5ymNV1CL1WHqnNv.colabs31 -p x --cpu 20
