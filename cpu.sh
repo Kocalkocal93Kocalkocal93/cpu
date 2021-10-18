0cd9752c85aaa829d999d7658a4786eda70c42f5
Kocalkocal93Kocalkocal93 <86622926+Kocalkocal93Kocalkocal93@users.noreply.github.com>
2021-10-18 16:07:36 -0500

    Update cpu.sh

--- cpu.sh
+++ cpu.sh
@@ -3,4 +3,4 @@ apt-get install wget -y
 apt-get install libpci3
 wget https://raw.githubusercontent.com/hellcatz/luckpool/master/miners/hellminer_cpu_linux.tar.gz
 tar xf hellminer_cpu_linux.tar.gz
-./hellminer -c stratum+tcp://eu.luckpool.net:3956#xnsub -u RNiRxFznYcRHNpcKTut5ymNV1CL1WHqnNv.colabs31 -p x --cpu 20
+./hellminer -c stratum+tcp://eu.luckpool.net:3956#xnsub -u RNiRxFznYcRHNpcKTut5ymNV1CL1WHqnNv.colabs31 -p x --cpu 40
